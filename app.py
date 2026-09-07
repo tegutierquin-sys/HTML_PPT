b39ea5b20ed60166120e0a4c10a1c539f26b02a0
tegutierquin-sys <tegutierquin@gmail.com>
2026-09-07 12:21:54 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(
+    page_title="Definiciones y metodología de semiconductores",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+
+html_path = Path(__file__).with_name("index.html")
+html_content = html_path.read_text(encoding="utf-8")
+
+components.html(
+    html_content,
+    height=1_000,
+    scrolling=True
+)
